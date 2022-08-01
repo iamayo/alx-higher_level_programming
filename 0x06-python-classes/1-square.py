154630fc4d16e02d663950524b9025d40a00e987
iamayo <iamjerrydev@gmail.com>
2022-08-01 05:57:05 -0700

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Creates an empty class called square """
+
+
 class Square:
-    """Represents a square.
+    """ Represents a square.
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
