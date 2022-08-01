5342cbed0564cc4fee9885817c46f59b72182ee2
iamayo <iamjerrydev@gmail.com>
2022-08-01 08:46:24 -0700

    square Advance

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 class Node:
     """Node of a singly linked list.
     Private instance attribute: data:
--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Creates an empty class called Square """
+
+
 class Square:
     """Represents a square.
     Private instance attribute: size:
